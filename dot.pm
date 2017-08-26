116449593d324fda64a4061b42ac64a0839b1012
Yang Bo <rslovers@yandex.com>
2017-08-26 16:26:28 +0000

    Modifiy everything that mattered to use the new interface.

--- dot.pm
+++ dot.pm
@@ -14,10 +14,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package dot;
-use warnings;
-use strict;
-use c;
-our @A = qw/Scalar::Util/;
+use c 'sane', 'autoload' => ['Scalar::Util'];
 sub add {
 	my $h = shift;
 	while (@_) {
