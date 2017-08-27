10a178e75c5292a7bb69f67c67672709e8143512
Yang Bo <rslovers@yandex.com>
2017-08-27 00:31:42 +0000

    Split autoload into inner and outter ones.

--- dot.pm
+++ dot.pm
@@ -14,7 +14,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 package dot;
-use c 'sane', 'autoload' => ['Scalar::Util'];
+use c 'sane', iautoload => ['Scalar::Util'];
 sub add {
 	my $h = shift;
 	while (@_) {
