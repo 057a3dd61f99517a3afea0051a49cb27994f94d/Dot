e94437217d065946084e32e2a9f7dabe13371110
Yang Bo <rslovers@yandex.com>
2018-06-12 13:08:59 +0000

    Release v1.0.1.
    Use quoted version

--- Dot.pm
+++ Dot.pm
@@ -13,9 +13,10 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 package Dot;
 
-our $VERSION = v1;
+our $VERSION = 'v1.0.1';
 
 use strict;
 use warnings qw/all FATAL uninitialized/;
